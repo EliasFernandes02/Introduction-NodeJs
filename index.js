20ee9214d6509087b471c80d14e9296baef72e1e
EliasFernandes02 <eliasneto0014@gmail.com>
2023-02-27 11:21:25 -0300

    creating routes

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const express = require("express");
 //iniciando e passando para variavel
 const app = express()
 
+ //rota
+
+ app.get("/",function(req,res){
+    res.send("Bem vinde")
+ });
+
+ 
+ app.get("/blog",function(req,res){
+    res.send("Saia do meu blog cara")
+ });
+
+ 
+ app.get("/canal/youtube",function(req,res){
+    res.send("Bem vinde to mai canal")
+ });
 //primeiro parametro e a port ado servidor o segundo e o tratamento de erros,subindo o servidor
 app.listen(4000,function(error){
     if(error) {
